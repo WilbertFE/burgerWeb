6b447fcdbe82d79583b26c14b29bd6be5827004f
WilbertFE <wilbertbernadi699@gmail.com>
2023-12-14 10:26:38 +0700

    Home section selesai

--- script/main.js
+++ script/main.js
@@ -4,7 +4,4 @@ const navigation = document.querySelector('.navigation');
 
 toggleButton.addEventListener('click', function(){
     navigation.classList.toggle('geser');
-    console.log('ok');
 });
-
-console.log('mulai');
